a565f1d18ee368464a5dfcaf41acc5c55ee15568
gustavoleopoldes <leopoldesgustavo@gmail.com>
2026-01-10 14:49:22 -0300

    test_w1

--- server.js
+++ server.js
@@ -6,13 +6,17 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
- * Routes
+ * View Engine and Templates
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") //not at views root
 app.use(static)
 
 /* ***********************
@@ -27,4 +31,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
 })
